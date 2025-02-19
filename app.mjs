98333ea69a342051da0028d39a5b0418b78c799d
Tayyabakhatri <Tayyabakhatri225@gmail.com>
2025-02-19 07:22:24 +0500

    modified

--- app.mjs
+++ app.mjs
@@ -1,1 +1,13 @@
-console.log("tayyaba");
+const express = require('express')
+const app = express()
+const port = process.evn.PORT||3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+  console.log(req.ip);
+  
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
