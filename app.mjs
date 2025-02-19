4a6fd8a8d7a03c93e382888b7ab49151b5708d79
Tayyaba khatri <157671285+Tayyabakhatri@users.noreply.github.com>
2025-02-19 09:35:35 +0500

    Update app.mjs

--- app.mjs
+++ app.mjs
@@ -1,6 +1,6 @@
 import express from 'express'
 const app = express()
-const port = process.evn.PORT||3000
+const port = process.env.PORT||3000
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
