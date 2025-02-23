33635b20c8f8dd8228f7e43beea874cd8c421818
Tayyabakhatri <Tayyabakhatri225@gmail.com>
2025-02-23 09:57:07 +0500

    ..

--- web/src/Component/QuizApp.jsx
+++ web/src/Component/QuizApp.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
-import { Card, CardContent } from "@/components/ui/card";
-import { Button } from "@/components/ui/button";
-import { Progress } from "@/components/ui/progress";
+import { Card, CardContent } from "/components/ui/card";
+import { Button } from "/components/ui/button";
+import { Progress } from "/components/ui/progress";
 import { motion } from "framer-motion";
 import { CheckCircle, XCircle } from "lucide-react";
 
